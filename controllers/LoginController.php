004388bf5e445996015fc4a85df14386a4eeaea3
Michael Vallejos <maikol8-10@hotmail.com>
2022-03-17 16:56:31 -0600

    Estructura SPA con PHP 8, NPM, SASS

---
+++ controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController
+{
+    public static function login(Router $router)
+    {
+        $router->render('auth/login');
+        //echo "Desde Login";
+    }
+
+    public static function logout()
+    {
+        echo "Desde Logout";
+    }
+
+    public static function olvide()
+    {
+        echo "Desde Olvide";
+    }
+
+    public static function recuperar()
+    {
+        echo "Desde Recuperar";
+    }
+
+    public static function crear()
+    {
+        echo "Desde Crear";
+    }
+}
